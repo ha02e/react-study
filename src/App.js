6939bf3e785ddfc6e9d4db346b0682bf051ba7da
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-10-28 20:44:40 +0900

    231028_JSX 문법 CSS 스타일링 인라인

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
-import "./App.css";
+//import "./App.css";
 
 import MyHeader from "./MyHeader";
 
 function App() {
   let name = "이하영";
 
+  const style = {
+    App: {
+      backgroundColor: "black",
+    },
+    h2: {
+      color: "red",
+    },
+    bold_text: {
+      color: "green",
+    },
+  };
+
   return (
-    <div className="App">
+    <div style={style.App}>
       <MyHeader />
-      <h2>안녕 리액트 {name}</h2>
-      <b id="bold_text">React.js</b>
+      <h2 style={style.h2}>안녕 리액트 {name}</h2>
+      <b style={style.bold_text} id="bold_text">
+        React.js
+      </b>
     </div>
   );
 }
