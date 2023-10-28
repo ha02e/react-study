57495bedc66f00f46a44d3fbd6bf8a7ce05ab4fa
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-10-28 20:31:43 +0900

    231028_MyHeader, MyFooter 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 
+import MyHeader from "./MyHeader";
+
 function App() {
   return (
     <div className="App">
+      <MyHeader />
       <header className="App-header">
         <h2>안녕 리액트</h2>
       </header>
