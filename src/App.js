a624e0644a1e388212d19b1953952f3682ceea4b
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-10-28 22:57:28 +0900

    231028_React State 실습

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import React from "react";
+import Counter from "./Counter";
 //import "./App.css";
 
 import MyHeader from "./MyHeader";
 
 function App() {
-  //let name = "이하영";
-
-  const style = {
-    App: {
-      backgroundColor: "black",
-    },
-    h2: {
-      color: "red",
-    },
-    bold_text: {
-      color: "green",
-    },
-  };
-
-  const number = 5;
-
   return (
-    <div style={style.App}>
+    <div>
       <MyHeader />
-      <h2 style={style.h2}>안녕 리액트</h2>
-      <b style={style.bold_text} id="bold_text">
-        {number}는 : {number % 2 === 0 ? "짝수" : "홀수"}
-      </b>
+      <Counter />
     </div>
   );
 }
